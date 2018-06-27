b562d2d1a3650abaa161343501b81c1a83ee72a4
wellington.zaneze <wellington.zaneze@dextra-sw.com>
2018-06-27 17:43:08 -0300

    add component model

---
+++ IoTPlant/Models/Component.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPlant.Models
+{
+    public class Component : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        SerialPort port;
+
+        public IoTPlant plant = new IoTPlant();
+        public IoTPlant Plant
+        {
+            get
+            {
+                return this.plant;
+            }
+            set
+            {
+                if (this.plant != value)
+                {
+                    this.plant = value;
+                    if (this.PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Plant"));
+                    }
+                }
+            }
+        }
+
+        public Component(string port)
+        {
+            this.port = new SerialPort(port, 9600);
+
+            Task readData = new Task(ReadData);
+            readData.Start();
+        }
+
+        void ReadData()
+        {
+            using (port)
+            {
+                port.Open();
+                while (port.IsOpen)
+                {
+                    string result = port.ReadLine();
+
+                    try
+                    {
+                        Plant = JsonConvert.DeserializeObject<IoTPlant>(result);
+                    }
+                    catch
+                    {
+                        Plant = new IoTPlant();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IoTPlant/Models/IoTPlant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPlant.Models
+{
+    public class IoTPlant
+    {
+        public int Umidade { get; set; }
+        public int Temperatura { get; set; }
+        public string Solo { get; set; }
+    }
+}
